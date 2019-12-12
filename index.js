fa557cbea929acb5cc4681a2128f8486cebd4dab
srgbrs <borisov11092000@gmail.com>
2019-12-12 20:09:27 +0200

    start script updated

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
+
+
 const http = require('http')
+const fs = require("fs");
 const hostname = '127.0.0.1'
 const port = 3000
 const server = http.createServer((req, res) => 
-{ res.statusCode = 200 
-    res.setHeader('Content-Type', 'text/plain') 
-    res.end('Hi there!\n')
+{  res.statusCode = 200 
+    res.setHeader('Content-Type', 'text/html') 
+    
+
+    fs.readFile("./lab5.html", null, (error, data) => {
+        if (error) {
+            res.writeHead(404);
+          res.write("Whoops! File not found!");
+        } else {
+          res.write(data);
+        }
+        res.end();
+        });
+
 })
 server.listen(port, hostname, () => {
 console.log(`Server running at http://${hostname}:${port}/`) })
