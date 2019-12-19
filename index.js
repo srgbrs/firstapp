9dec20a354b8842be39f1bda7ea86116860e8a49
srgbrs <borisov11092000@gmail.com>
2019-12-20 01:06:54 +0200

    fix8

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const http = require('http')
 const fs = require("fs");
- const hostname = '127.0.0.1'
+ //const hostname = '127.0.0.1'
 // const port = 3000
 const server = http.createServer((req, res) => 
 {  res.statusCode = 200 
@@ -20,5 +20,5 @@ const server = http.createServer((req, res) =>
         });
 
 })
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`) })
+server.listen(PORT, () => {
+console.log(`Server running `) })
